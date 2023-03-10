d79776d73166a772ef0468b662373838c16e084c
Nischal Jagadish Mudennavar <nischalmudennavar@gmail.com>
2023-03-10 21:06:04 +0530

    new commit

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
-
 import bodyParser from "body-parser";
-
 import usersRoutes from "./routes/users.js";
 
 const app = express();
-
 const PORT = 5000;
 
 app.use(bodyParser.json());
-
 app.use("/users", usersRoutes); // use the usersRoutes for all routes starting with /users
 
 // test route
